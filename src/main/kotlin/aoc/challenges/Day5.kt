aebfadc65516a04924b17ba1e9ced3e5e6723617
Hc747 <harrisoncole05@gmail.com>
2018-12-06 14:17:59 +1100

    Refactoring

--- src/main/kotlin/aoc/challenges/Day5.kt
+++ src/main/kotlin/aoc/challenges/Day5.kt
@@ -9,30 +9,23 @@ import kotlin.math.max
  * @version 1.0
  * @since 2018-12-05
  */
-class Polymer(val units: String) {
+fun String.react(): String {
+    val builder = StringBuilder(this)
+    var index = 0
 
-    fun react(): String {
-        val builder = StringBuilder(units)
-        var index = 0
+    while (index < builder.length - 1) {
+        val current = builder[index]
+        val next = builder[index + 1]
 
-        while (index < builder.length) {
-            val current = builder[index]
-
-            if (index + 1 >= builder.length) break
-
-            val next = builder[index + 1]
-
-            if (current.equals(next, ignoreCase = true) && current != next) {
-                builder.deleteCharAt(index + 1).deleteCharAt(index)
-                index = max(0, --index)
-            } else {
-                index++
-            }
+        if (current.equals(next, ignoreCase = true) && current != next) {
+            builder.delete(index, index + 2)
+            index = max(0, --index)
+        } else {
+            index++
         }
-
-        return builder.toString()
     }
 
+    return builder.toString()
 }
 
 class Day5 : DailyChallenge<String, Int, Int> {
@@ -40,13 +33,12 @@ class Day5 : DailyChallenge<String, Int, Int> {
     override val input = AdventUtils.readText(5).trim()
 
     override fun first(): Int {
-        return Polymer(input).react().length
+        return input.react().length
     }
 
     override fun second(): Int {
         return ('a'..'z').map { char ->
-            val units = input.filter { !it.equals(char, ignoreCase = true) }
-            Polymer(units).react().length
+            input.filter { !it.equals(char, ignoreCase = true) }.react().length
         }.min()!!
     }
 
